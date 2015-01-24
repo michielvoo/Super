385fd5334be500d82acc97639fe7a3e846c76156
Michiel van Oosterhout <michielvoo@gmail.com>
2015-01-24 21:39:25 +0100

    Workaround for segfaults in wlalink.

--- src/build.sh
+++ src/build.sh
@@ -22,13 +22,32 @@ obj_file="$project_dir/${program_name%.*}.o"
 link_file="$project_dir/link.txt"
 rom_file="$project_dir/$project_name.rom"
 
+
+# Remove previous build artifacts
+
+rm -f "$obj_file"
+rm -f "$rom_file"
+
 # Assemble program
 
 pushd "$project_dir" 1> /dev/null
 
 PATH=$PATH:/usr/local/bin
 wla-65816 -oiv "$program_file" "$obj_file" || { exit 1; }
-wlalink -irv "$link_file" "$rom_file" || { rm "$obj_file"; exit 1; }
+
+wlalink -irv "$link_file" "$rom_file"
+while [ $? -eq 139 ];
+do
+    # wlalink segfaults intermittently
+    wlalink -irv "$link_file" "$rom_file"
+done
+
+if [ $? -gt 0 ];
+then
+    # Remove intermediate build artifacts
+    rm -f "$obj_file"
+    exit 1
+fi
 
 popd 1> /dev/null
 
@@ -38,7 +57,7 @@ build_script_dir="${build_script_file%/*}"
 upload_script_file="$build_script_dir/upload.sh"
 
 # Upload ROM file to cartridge or emulator for testing
-if [ -f "$upload_script_file" ];
+if [ -f "$upload_script_file" -a -f "$rom_file" ];
 then
     "$upload_script_file" "$rom_file"
 fi
