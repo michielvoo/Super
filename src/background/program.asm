6a2d1c114e46d031c0cf1bf1b8a12f32248c8ced
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-04 21:39:51 +0100

    Switched to 16 by 16 pixel tiles

--- src/background/program.asm
+++ src/background/program.asm
@@ -20,6 +20,18 @@ Reset:
 
     sep #$20
 
+; Background
+
+    lda #$10
+    sta BGMODE  ; Mode 0, large tiles
+
+    lda #$04
+    sta BG1SC   ; Tilemap segment 1 (address $0400)
+
+    stz BG12NBA ; Character segment 0
+
+; Color
+
     lda #$01    ; Palette 0, color 1
     sta CGADD
     lda #$FF
@@ -27,30 +39,93 @@ Reset:
     lda #$7F
     sta CGDATA
 
+; Character
 
-    lda #$00
+    lda #$00    ; Skip the high bytes when writing the character (plane 0)
     sta VMAIN
 
     ldx #$08    ; Character segment 0, character 1
     stx VMADD
 
+    ; A
     lda #%00000000
     sta VMDATAL
-    lda #%01111111
+    lda #%00000000
     sta VMDATAL
-    lda #%01000001
+    lda #%00111111
     sta VMDATAL
-    lda #%01000001
+    lda #%00100000
     sta VMDATAL
-    lda #%01000001
+    lda #%00100000
     sta VMDATAL
-    lda #%01000001
+    lda #%00100000
     sta VMDATAL
-    lda #%01000001
+    lda #%00100000
     sta VMDATAL
-    lda #%01111111
+    lda #%00100000
     sta VMDATAL
 
+    ; B
+    lda #%00000000
+    sta VMDATAL
+    lda #%00000000
+    sta VMDATAL
+    lda #%11111110
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+
+    ; C
+    lda #($08 + (16 * 8) )
+    sta VMADD
+
+    lda #%00100000
+    sta VMDATAL
+    lda #%00100000
+    sta VMDATAL
+    lda #%00100000
+    sta VMDATAL
+    lda #%00100000
+    sta VMDATAL
+    lda #%00100000
+    sta VMDATAL
+    lda #%00100000
+    sta VMDATAL
+    lda #%00111111
+    sta VMDATAL
+    lda #%00000000
+    sta VMDATAL
+
+    ; D
+    lda #%10000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%00000010
+    sta VMDATAL
+    lda #%11111110
+    sta VMDATAL
+    lda #%00000000
+    sta VMDATAL
+
+
+; Tilemap
 
     lda #$80
     sta VMAIN
@@ -60,19 +135,13 @@ Reset:
     ldx #$0400  ; Tilemap segment 1
     stx VMADD
     lda #$01    ; Character 1, palette 0
-    ldx #$0400
+    ldx #$0400  ; 32 by 32 tilemap
 -   sta VMDATAL
     stz VMDATAH
     dex
     bne -
 
-
-    stz BGMODE  ; Mode 0
-
-    lda #$04
-    sta BG1SC   ; Tilemap segment 1 (address $0400)
-
-    stz BG12NBA ; Character segment 0
+; Enable
 
     lda #$01
     sta TM      ; Background layer 1
