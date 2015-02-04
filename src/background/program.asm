07aadd7699496a0140ed9428a27e43c7bcc74874
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-04 20:35:06 +0100

    Draws a square for tile 0, background layer 0

--- src/background/program.asm
+++ src/background/program.asm
@@ -12,19 +12,72 @@ Reset:
     clc
     xce
 
-    .INCDIR "../lib/"
+    rep #$08
+
+    .INCDIR "../lib"
     .INCLUDE "registers.asm"
     .INCLUDE "initialize.asm"
 
-    stz CGADD
-    lda #$57
+    sep #$20
+
+    lda #$01    ; Palette 0, color 1
+    sta CGADD
+    lda #$FF
     sta CGDATA
+    lda #$7F
     sta CGDATA
+
+
+    lda #$00
+    sta VMAIN
+
+    ldx #$08    ; Character segment 0, character 1
+    stx VMADD
+
+    lda #%00000000
+    sta VMDATAL
+    lda #%01111111
+    sta VMDATAL
+    lda #%01000001
+    sta VMDATAL
+    lda #%01000001
+    sta VMDATAL
+    lda #%01000001
+    sta VMDATAL
+    lda #%01000001
+    sta VMDATAL
+    lda #%01000001
+    sta VMDATAL
+    lda #%01111111
+    sta VMDATAL
+
+
+    lda #$80
+    sta VMAIN
+
+    rep #$10
+
+    ldx #$0400  ; Tilemap segment 1
+    stx VMADD
+    lda #$01    ; Character 1, palette 0
+    sta VMDATA
+
+
+    stz BGMODE  ; Mode 0
+
+    lda #$04
+    sta BG1SC   ; Tilemap segment 1 (address $0400)
+
+    stz BG12NBA ; Character segment 0
+
+    lda #$01
+    sta TM      ; Background layer 1
+
 
     lda #$0F
     sta INIDISP
 
-    lda #(NMIENABLE | JOYENABLE)
+    lda #NMIENABLE
     sta NMITIMEN
 
 -   wai
