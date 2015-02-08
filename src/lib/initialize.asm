3d90c35ff4ef8cc1fae8a091c951fca5115eaacd
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-09 00:08:01 +0100

    Fixed indentation

--- src/lib/initialize.asm
+++ src/lib/initialize.asm
@@ -1,13 +1,14 @@
-; Initializes registers and clears OAM, VRAM and CGRAM.
+Initialize:
+    ; Initializes registers and clears OAM, VRAM and CGRAM.
 
-sep #$20
+    sep #$20
 
-lda #$80
-sta INIDISP
+    lda #$80
+    sta INIDISP
 
-stz OBSEL
+    stz OBSEL
 
-; Clear OAMDATA
+    ; Clear OAMDATA
     stz OAMADDL
     stz OAMADDH
 
@@ -26,39 +27,40 @@ stz OBSEL
 
     stz OAMADDL
     stz OAMADDH
+    ; // Clear OAMDATA
 
-stz BGMODE
-stz MOSAIC
+    stz BGMODE
+    stz MOSAIC
 
-stz BG1SC
-stz BG2SC
-stz BG3SC
-stz BG4SC
+    stz BG1SC
+    stz BG2SC
+    stz BG3SC
+    stz BG4SC
 
-stz BG12NBA
-stz BG34NBA
+    stz BG12NBA
+    stz BG34NBA
 
-stz BG1HOFS
-stz BG1HOFS
-stz BG1VOFS
-stz BG1VOFS
+    stz BG1HOFS
+    stz BG1HOFS
+    stz BG1VOFS
+    stz BG1VOFS
 
-stz BG2HOFS
-stz BG2HOFS
-stz BG2VOFS
-stz BG2VOFS
+    stz BG2HOFS
+    stz BG2HOFS
+    stz BG2VOFS
+    stz BG2VOFS
 
-stz BG3HOFS
-stz BG3HOFS
-stz BG3VOFS
-stz BG3VOFS
+    stz BG3HOFS
+    stz BG3HOFS
+    stz BG3VOFS
+    stz BG3VOFS
 
-stz BG4HOFS
-stz BG4HOFS
-stz BG4VOFS
-stz BG4VOFS
+    stz BG4HOFS
+    stz BG4HOFS
+    stz BG4VOFS
+    stz BG4VOFS
 
-; Clear VMDATA
+    ; Clear VMDATA
     lda #$80
     sta VMAIN   ; Set VMDATA transfer mode to 16-bit
 
@@ -75,22 +77,23 @@ stz BG4VOFS
 
     stz VMADDL
     stz VMADDH
+    ; // Clear VMDATA
 
-stz M7SEL
-stz M7A
-stz M7A
-stz M7B
-stz M7B
-stz M7C
-stz M7C
-stz M7D
-stz M7D
-stz M7X
-stz M7X
-stz M7Y
-stz M7Y
+    stz M7SEL
+    stz M7A
+    stz M7A
+    stz M7B
+    stz M7B
+    stz M7C
+    stz M7C
+    stz M7D
+    stz M7D
+    stz M7X
+    stz M7X
+    stz M7Y
+    stz M7Y
 
-; Clear CGDATA
+    ; Clear CGDATA
     stz CGADD
 
     ldx #$0100
@@ -100,44 +103,45 @@ stz M7Y
     bne -
 
     stz CGADD
+    ; Clear CGDATA
 
-stz W12SEL
-stz W34SEL
-stz WOBJSEL
-stz WH0
-stz WH1
-stz WH2
-stz WH3
-stz WBGLOG
-stz WOBJLOG
+    stz W12SEL
+    stz W34SEL
+    stz WOBJSEL
+    stz WH0
+    stz WH1
+    stz WH2
+    stz WH3
+    stz WBGLOG
+    stz WOBJLOG
 
-stz TM
-stz TD
-stz TMW
-stz TSW
+    stz TM
+    stz TD
+    stz TMW
+    stz TSW
 
-stz CGWSEL
-stz CGADSUB
-lda #$E0
-sta COLDATA     ; Set BGR to 0
-stz SETINI
+    stz CGWSEL
+    stz CGADSUB
+    lda #$E0
+    sta COLDATA     ; Set BGR to 0
+    stz SETINI
 
-stz NMITIMEN
-lda #$FF
-sta WRIO        ; Enable RDIO
-stz WRMPYA
-stz WRMPYB
-stz WRDIVL
-stz WRDIVH
-stz WRDIVB
-stz HTIMEL
-stz HTIMEH
-stz VTIMEL
-stz VTIMEH
-stz MDMAEN
-stz HDMAEN
-stz MEMSEL
+    stz NMITIMEN
+    lda #$FF
+    sta WRIO        ; Enable RDIO
+    stz WRMPYA
+    stz WRMPYB
+    stz WRDIVL
+    stz WRDIVH
+    stz WRDIVB
+    stz HTIMEL
+    stz HTIMEH
+    stz VTIMEL
+    stz VTIMEH
+    stz MDMAEN
+    stz HDMAEN
+    stz MEMSEL
 
-stz WMADDL
-stz WMADDH
-stz WMADDB
+    stz WMADDL
+    stz WMADDH
+    stz WMADDB
