8558f2bee5460600148bf491d4a6b0157c92cb47
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-08 21:29:37 +0100

    Better comments

--- src/onoff/program.asm
+++ src/onoff/program.asm
@@ -1,5 +1,5 @@
 ; onoff
-; Turn the screen on and off again by pressing up and down respectively on the direction pad.
+; Turn the screen on and off by pressing up and down respectively on the direction pad.
 
 .INCLUDE "../lib/registers.asm"
 .INCLUDE "../lib/settings.asm"
@@ -12,36 +12,46 @@
 .SECTION "Main"
 
 Reset:
+    ; Switch to native mode
     clc
     xce
 
+    ; Initialize registers, OAM, CGDATA and VRAM
     .INCLUDE "../lib/initialize.asm"
 
+    ; Set color 0 of palette 0 to white
     stz CGADD
     lda #$FF
     sta CGDATA
+    lda #$7F
     sta CGDATA
 
+    ; Enable the screen at full brightness
     lda #$0F
     sta INIDISP
 
+    ; Enable the VBlank NMI and the joypad
     lda #(NMITIMEN_NMIENABLE | NMITIMEN_JOYENABLE)
     sta NMITIMEN
 
+    ; Keep waiting for the interrupts
 -   wai
     jmp -
 
 VBlank:
+    ; Wait until the joypad input can be read
     lda #HVBJOY_JOYREADY
 -   and HVBJOY
     cmp #HVBJOY_JOYREADY
     bne -
 
+    ; Jump to label On if the up button was pressed
     lda #JOYH_UP
     and JOY1H
     cmp #JOYH_UP
     beq On
 
+    ; Jump to label Off if the down button was pressed
     lda #JOYH_DOWN
     and JOY1H
     cmp #JOYH_DOWN
@@ -50,12 +60,14 @@ VBlank:
     rti
 
 On:
+    ; Turn on the screen at full brightness
     lda #$0F
     sta INIDISP
 
     rti
 
 Off:
+    ; Turn off the screen and set brightness to zero
     lda #$80
     sta INIDISP
 
