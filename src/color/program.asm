0be877e2bc49493474f4b8cccaa1fdca7fd59ef2
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-08 21:46:52 +0100

    Better comments

--- src/color/program.asm
+++ src/color/program.asm
@@ -1,5 +1,6 @@
 ; bgcolor
 ; Press the A, B, X and Y buttons to change the background color. 
+
 .INCLUDE "../lib/registers.asm"
 .INCLUDE "../lib/settings.asm"
 .INCLUDE "../lib/values.asm"
@@ -11,56 +12,69 @@
 .SECTION "Main"
 
 Reset:
+    ; Switch to native mode
     clc
     xce
 
     .INCLUDE "../lib/initialize.asm"
 
+    ; Set color 0 of palette 0 to white
     stz CGADD
     lda #$FF
     sta CGDATA
     lda #$7F
     sta CGDATA
 
+    ; Enable the screen at full brightness
     lda #$0F
     sta INIDISP
 
+    ; Enable the VBlank NMI and the joypad
     lda #(NMITIMEN_NMIENABLE | NMITIMEN_JOYENABLE)
     sta NMITIMEN
 
+    ; Keep waiting for the interrupts
 -   wai
     jmp -
 
 VBlank:
+    ; Wait until the joypad input can be read
     lda #HVBJOY_JOYREADY
 -   and HVBJOY
     cmp #HVBJOY_JOYREADY
     bne -
 
+    ; Prepare to change color 0 of palette 0
     stz CGADD
 
+    ; Set accumulator register to 16-bit
     rep #$20
 
+    ; Jump to label Yellow if the A and Y buttons were pressed
     lda #(JOY_RED + JOY_GREEN)
     and JOY1
     cmp #(JOY_RED + JOY_GREEN)
     beq Yellow
 
+    ; Jump to label Red if the A button was pressed
     lda #JOY_RED
     and JOY1
     cmp #JOY_RED
     beq Red
 
+    ; Jump to label Yellow if the B button was pressed
     lda #JOY_YELLOW
     and JOY1
     cmp #JOY_YELLOW
     beq Yellow
 
+    ; Jump to label Blue if the X button was pressed
     lda #JOY_BLUE
     and JOY1
     cmp #JOY_BLUE
     beq Blue
 
+    ; Jump to label Green if the Y button was pressed
     lda #JOY_GREEN
     and JOY1
     cmp #JOY_GREEN
@@ -69,8 +83,10 @@ VBlank:
     rti
 
 Red:
+    ; Set accumulator register back to 8-bit
     sep #$20
 
+    ; Set color 0 of palette 0 to red
     lda #%00011111
     sta CGDATA
     lda #%00000000
@@ -79,8 +95,10 @@ Red:
     rti
 
 Green:
+    ; Set accumulator register back to 8-bit
     sep #$20
 
+    ; Set color 0 of palette 0 to green
     lda #%11100000
     sta CGDATA
     lda #%00000011
@@ -89,8 +107,10 @@ Green:
     rti
 
 Blue:
+    ; Set accumulator register back to 8-bit
     sep #$20
 
+    ; Set color 0 of palette 0 to blue
     lda #%00000000
     sta CGDATA
     lda #%01111100
@@ -99,8 +119,10 @@ Blue:
     rti
 
 Yellow:
+    ; Set accumulator register back to 8-bit
     sep #$20
 
+    ; Set color 0 of palette 0 to yellow
     lda #%11111111
     sta CGDATA
     lda #%00000011
