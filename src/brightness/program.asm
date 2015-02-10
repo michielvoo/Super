89e9d2ce730643b4de0552096a3bdb60bc730474
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-10 07:10:19 +0100

    Start new program

---
+++ src/brightness/program.asm
@@ -0,0 +1,31 @@
+; brightness
+; Turn the brightness of the display up or down using the D-pad.
+
+.INCLUDE "header.asm"
+.INCLUDE "../lib/registers.asm"
+.INCLUDE "../lib/settings.asm"
+.INCLUDE "../lib/values.asm"
+.INCLUDE "../lib/initialization.asm"
+
+.BANK 0
+.ORG 0
+
+.SECTION "Main" SEMIFREE
+
+Reset:
+    RESET
+
+    ; Set accumulator to 8-bit mode
+    sep #$20
+
+    ; Enable VBlank
+    lda #NMITIMEN_NMI_ENABLE
+    sta NMITIMEN
+
+-   wai
+    jmp -
+
+VBlank:
+    rti
+
+.ENDS
