3247f261009e2800acbddccad51dd1d1f6009967
Michiel van Oosterhout <michielvoo@gmail.com>
2015-02-24 21:31:53 +0100

    Sort of works

--- examples/mosaic/program.asm
+++ examples/mosaic/program.asm
@@ -22,21 +22,22 @@ Main:
     lda #$05
     sta CGADD
 
-    ; Color 1 (red)
-    lda #%00011111
+    ; Color 1
+    lda #%00111001
     sta CGDATA
-    stz CGDATA
-
-    ; Color 2 (green)
-    lda #%11100000
-    sta CGDATA
-    lda #%00000011
+    lda #%00000000
     sta CGDATA
 
-    ; Color 3 (blue)
-    lda #$00
+    ; Color 2
+    lda #%00100000
     sta CGDATA
-    lda #%01111100
+    lda #%00000111
+    sta CGDATA
+
+    ; Color 3
+    lda #%00000001
+    sta CGDATA
+    lda #%01100100
     sta CGDATA
 
     ; Set background mode to 0 and tilesize to 8 by 8 pixels
@@ -57,7 +58,7 @@ Main:
     lda #$08
     sta VMADDL
 
-    lda #%11000000
+    lda #%10000000
 
     ; Character 1 (top-left pixel color 1)
     sta VMDATAL
@@ -155,6 +156,10 @@ Main:
     dex
     bne -
 
+    ; Move background layer 1 down 1 scanline
+    lda #$01
+    sta BG1VOFS
+
     ; Enable background layer 1 on the main screen
     lda #$01
     sta TM
@@ -171,8 +176,11 @@ Main:
     .DEFINE INPUT $7F0000
     .DEFINE INPUT_PREVIOUS $7F0002
 
-    ; Push mosaic size
-    lda #$00
+    ; Enable mosaic effect for background layer 1
+    lda #$01
+    sta MOSAIC
+
+    ; Push mosaic settings
     pha
 
 -   wai
@@ -215,19 +223,14 @@ Up:
     ; Set accumulator to 8-bit mode
     sep #$20
 
+    ; Pull mosaic settings
     pla
     
-    cmp #%00001111
+    cmp #%11110001
     beq +
-    inc a
-    pha
-
-    asl
-    asl
-    asl
-    asl
-    ora #$01
+    adc #%00010000
     sta MOSAIC
+    pha
 
 +   rti
 
@@ -237,17 +240,11 @@ Down:
 
     pla
 
-    cmp #$00
+    cmp #%00000001
     beq +
-    dec a
-    pha
-
-    asl
-    asl
-    asl
-    asl
-    ora #$01
+    sbc #%00010000
     sta MOSAIC
+    pha
 
 +   rti
 
