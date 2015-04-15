a913b8854ea0ce0a2766d041d0f53f9f8ff28eb8
Michiel van Oosterhout <michielvoo@gmail.com>
2015-04-15 17:53:05 +0200

    Refactored validation into separate method

--- tools/graphics/gif.py
+++ tools/graphics/gif.py
@@ -11,25 +11,29 @@ class Image(object):
         except struct.error:
             raise ValueError("Invalid GIF file")
 
+        # Initialize fields
         self._id = header[0]
         self._version = header[1]
+        self.dimensions = (header[2], header[3])
+        self._fields = header[4]
+        self._color_depth = (self._fields & 0x07) + 1
         
+        self._validate()
+
+    """ Performs several checks on the GIF file to determine if it is supported. This method will 
+        raise an exception if the GIF file uses unsupported features.
+    """
+    def _validate(self):
         if self._id != "GIF" or self._version != "89a":
             raise ValueError("Unsupported GIF image, only GIF89a is supported")
 
-        self.dimensions = (header[2], header[3])
-
         if any(d > 64 for d in self.dimensions):
             raise ValueError("Unsupported GIF image, dimensions may not exceed 64 by 64 pixels")
-
-        self._fields = header[4]
-
+        
         if not self._fields & 0x80:
             raise ValueError("Unsupported GIF image, no global color table")
 
-        # 'Isolate' the last 3 bits by AND-ing with 
-        self._color_depth = (self._fields & 0x07) + 1
-
         if self._color_depth > 2:
             raise ValueError("Unsupported GIF image, too many colors")
+
 #
